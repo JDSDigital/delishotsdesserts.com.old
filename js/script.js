16dd3afad60750b40972e560a52486bd616e8dd6
Daniel Sosa <jdsosa@gmail.com>
2016-07-23 13:40:48 -0430

    Add Product page and a smooth scroll with JQuery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-$(window).load(function() {
-  $("body").removeClass("preload");
+/*$('.product-nav').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+});*/
+
+$(function() {
+  $('.product-nav').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
 });
