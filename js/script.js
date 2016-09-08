f364d97ce43eab835df2bd44806ec27a5410e056
Daniel Sosa <jdsosa@gmail.com>
2016-09-08 15:02:12 -0400

    Fix products scroll and change fonts

--- js/script.js
+++ js/script.js
@@ -27,8 +27,6 @@ $(function() {
   });
 });
 
-$(".nuestrosProductos").stikify({rate: 3.4, cieling: -100});
-
 function swapImages(){
     var $active = $('#myGallery .active');
     var $next = ($('#myGallery .active').next().length > 0) ? $('#myGallery .active').next() : $('#myGallery img:first');
@@ -37,3 +35,35 @@ function swapImages(){
     $next.fadeIn().addClass('active');
     });
 }
+
+/* $(".nuestrosProductos").stikify({rate: .4, cieling: -100}); */
+
+$.fn.moveIt = function(){
+  var $window = $(window);
+  var instances = [];
+  
+  $(this).each(function(){
+    instances.push(new moveItItem($(this)));
+  });
+  
+  window.onscroll = function(){
+    var scrollTop = $window.scrollTop();
+    instances.forEach(function(inst){
+      inst.update(scrollTop);
+    });
+  }
+}
+
+var moveItItem = function(el){
+  this.el = $(el);
+  this.speed = parseInt(this.el.attr('data-scroll-speed'));
+};
+
+moveItItem.prototype.update = function(scrollTop){
+  var pos = scrollTop / this.speed;
+  this.el.css('transform', 'translateY(' + -pos + 'px)');
+};
+
+$(function(){
+  $('[data-scroll-speed]').moveIt();
+});
