674e525e454b098d762b3d41633879c629465f3f
Daniel Sosa <jdsosa@gmail.com>
2016-10-17 14:14:41 -0400

    Add first step of shopping cart

--- js/script.js
+++ js/script.js
@@ -12,6 +12,45 @@ $(document).ready(function(){
 	setInterval('swapImages2()', 3000);
 	setInterval('swapImages3()', 3500);
 
+/************************* Escoge tu mejor opción ****************************/
+
+	$('.add-product li').click(function() {
+
+		$('.show-product').addClass('current');
+		addProduct();
+
+	});
+
+	$('#product-list li').click(function() {
+
+		$("li").on("click", removeProduct);
+
+	});
+
+	var i = 1;
+
+	function addProduct() {
+
+	  var ul = document.getElementById("product-list");
+	  var li = document.createElement("li");
+		li.setAttribute("id", i);
+	  ul.appendChild(li);
+		$(li).append("Texto "+i);
+		i++;
+
+	}
+
+	function removeProduct() {
+
+		var element = document.getElementById(this.id);
+		element.parentNode.removeChild(element);
+
+	}
+
+
+
+/*********************** Fin de escoge tu mejor opción ***********************/
+
 })
 
 function swapImages(){
