7b6bdacea3066e4df0140021a76d5996ff6c2665
Daniel Sosa <jdsosa@gmail.com>
2016-10-18 11:53:58 -0400

    Add dropdown list to shopping cart

--- js/cart.js
+++ js/cart.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
 	$('#product-list').click(function() {
 
-		$("li").on("click", removeProduct);
+		//$("li").on("click", removeProduct);
 
 	});
 
@@ -20,10 +20,37 @@ $(document).ready(function(){
 
 	  var ul = document.getElementById("product-list");
 	  var li = document.createElement("li");
-		li.setAttribute("id", i);
+		li.setAttribute("id", "product"+i);
 	  ul.appendChild(li);
-		//$(li).append("Texto "+i);
-    $(li).append(prices[i].product);
+
+		$(li).append("<select id='selectProduct"+i+"'><option value='0'>Seleccione un producto</option></select><select id='selectType"+i+"'><option value='0'>Seleccione una presentación</option></select><select id='selectQuantity"+i+"'><option value='0'>Seleccione la cantidad</option></select>");
+
+		var selectProductLi = "#selectProduct"+i;
+		$("#selectProduct"+i).on("change", {name: selectProductLi}, createSelectType);
+		
+		document.getElementById("selectType"+i).addEventListener("change", createSelectQuantity);
+		document.getElementById("selectQuantity"+i).addEventListener("change", createEnding);
+
+		// Get dropdown element from DOM
+		var dropdown = document.getElementById("selectProduct"+i);
+
+		// Loop through the array
+		for (var j = 0; j < prices.length; ++j) {
+		    // Append the element to the end of Array list
+		    dropdown[dropdown.length] = new Option(prices[j].product, prices[j].product);
+		}
+
+		function createSelectType(event) {
+			console.log(event.data.name);
+		}
+
+		function createSelectQuantity() {
+
+		}
+
+		function createEnding() {
+		}
+
 		i++;
 
 	}
