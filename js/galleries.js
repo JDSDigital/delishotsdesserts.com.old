75ad7adab39f7f66e6e5dd0c4b90697eec9e4d98
Daniel Sosa <jdsosa@gmail.com>
2017-01-21 21:36:30 -0400

    Added iLightBox to the galleries

--- js/galleries.js
+++ js/galleries.js
@@ -59,3 +59,20 @@ jQuery(document).ready(function ($) {
     $(window).bind("orientationchange", ScaleSlider);
     //responsive code end
 });
+
+$(document).ready(function(e) {
+  $('a[data-lightbox-gallery]').iLightbox({
+    type: 'image', //'image', 'inline', 'ajax', 'iframe', 'swf' and 'html'
+    loop: true,
+    arrows: true,
+    closeBtn: true,
+    title: null,
+    href: null,
+    content: null,
+    beforeShow: function(a, b){ },
+    onShow: function(a, b){ },
+    beforeClose: function(){ },
+    afterClose: function(){ },
+    onUpdate: function(a){ },
+  });
+});
