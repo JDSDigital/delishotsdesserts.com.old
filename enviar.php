45655b47afe7aee1139126d26734d0364021daef
Daniel Sosa <jdsosa@gmail.com>
2017-01-29 20:10:49 -0400

    Changed page layout

--- enviar.php
+++ enviar.php
@@ -15,76 +15,107 @@
 	<link href="https://fonts.googleapis.com/css?family=Cinzel+Decorative:400,700|Raleway:400,400i|Oxygen" rel="stylesheet">
 </head>
 <body class="body">
-<div class="container-fluid">
+<div class="container">
 
-	<div class="row">
-		<header>
-			<a href="./index.html" class="hvr-ripple-out"><img id="logo-banner" class="img-responsive" src="./img/logo3.png" /></a>
-		</header>
-	</div>
-
-	<div class="row-fluid">
-		<nav id="top-menu">
-			<ul>
-				<li class=""><a class="hvr-wobble-top" href="./about.html">Quienes Somos</a></li>
-				<li class=""><a class="hvr-wobble-top" href="./nuestros_productos.html">Nuestros Productos</a></li>
-				<li class=""><a class="hvr-wobble-top" href="./opcion.html">Escoge tu Mejor Opción</a></li>
-				<li class=""><a class="hvr-wobble-top" href="./galeria.html">Galería</a></li>
-				<li class=""><a class="hvr-wobble-top active" href="./contactanos.html">Contáctanos</a></li>
-			</ul>
-		</nav>
-	</div>
-
-	<div class="row-fluid fade-in body">
-		<div class="col-md-8 col-md-offset-2 col-padding">
+	<div class="row fade-in body">
+		<div class="col-md-2 pr0">
+			<header>
+				<a href="./index.html" class="hvr-ripple-out"><img id="logo-banner" class="img-responsive" src="./img/logo.png" /></a>
+			</header>
+			<nav id="top-menu">
+				<ul>
+					<li class=""><a class="hvr-wobble-top" href="./index.html">Inicio</a></li>
+					<li class=""><a class="hvr-wobble-top" href="./about.html">Quienes Somos</a></li>
+					<li class=""><a class="hvr-wobble-top" href="./nuestros_productos.html">Nuestros Productos</a></li>
+					<!-- <li class=""><a class="hvr-wobble-top" href="./opcion.html">Escoge tu Mejor Opción</a></li> -->
+					<li class=""><a class="hvr-wobble-top" href="./galeria.html">Galería</a></li>
+					<li class=""><a class="hvr-wobble-top active" href="./contactanos.html">Contáctanos</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="col-md-9 col-padding">
 			<main id="main-index">
 
 				<h1>Contáctanos</h1>
 
-				<div class="row-fluid contact">
-					<form class="contact-form" action="enviar.php" method="post">
-						<input name="nombre" type="text" class="form-control form-text" placeholder="Nombre" required />
-						<input name="telefono" type="text" class="form-control form-text" placeholder="Teléfono" required />
-						<input name="correo" type="email" class="form-control form-text" placeholder="Correo" required />
-						<textarea name="comentario" class="form-control" rows="4" placeholder="Comentario" required></textarea>
-						<button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-send"></span> Enviar</button>
-					</form>
+				<div class="row contact">
 
-					<?php
-					$nombre = $_POST['nombre'];
-					$apellido = $_POST['apellido'];
-					$email = $_POST['email'];
-					$comentario = $_POST['comentario'];
+					<div class="col-sm-10 col-sm-offset-1 contact-data">
+						<div class="row">
+							<div class="col-lg-5 col-lg-offset-1 col-sm-12">
+								<p><span class="glyphicon glyphicon-earphone"></span> 0424-2777546</p>
+								<p><span class="glyphicon glyphicon-earphone"></span> 0424-2788219</p>
+							</div>
+							<div class="col-lg-6 col-sm-12">
+								<p><span class="glyphicon glyphicon-envelope"></span> <a href="mailto:delishotsdesserts@gmail.com">delishotsdesserts@gmail.com</a></p>
+							</div>
+						</div>
+					</div>
 
-					# Include the Autoloader (see "Libraries" for install instructions)
-					require 'vendor/autoload.php';
-					use Mailgun\Mailgun;
+					<div class="col-sm-10 col-sm-offset-1">
+						<h3>¡Déjanos tu comentario!</h3>
+						<form class="contact-form" action="enviar.php" method="post">
+							<div class="row">
 
-					# Instantiate the client.
-					$mgClient = new Mailgun('key-bb6be62102fbde6cb67a14a09ef126fe');
-					$domain = "mg.delishotsdesserts.com";
+								<div class="col-sm-4">
+									<input name="nombre" type="text" class="form-control form-text" placeholder="Nombre" required />
+								</div>
 
-					# Make the call to the client.
-					$result = $mgClient->sendMessage($domain, array(
-							'from'    => 'Grupo JGM Web <postmaster@mg.delishotsdesserts.com>',
-							'to'      => 'Grupo JGM <jdsosa@gmail.com>',
-							'subject' => 'Nuevo mensaje desde la página web',
-							'text'    => '
-									Ha recibido un nuevo mensaje desde la página web.
+								<div class="col-sm-4">
+									<input name="telefono" type="text" class="form-control form-text" placeholder="Teléfono" required />
+								</div>
 
-									Nombre: ' .$nombre .'
-									Apellido: ' .$apellido .'
-									Email: ' .$email .'
-									Comentario: ' .$comentario .'
-							'
-					));
+								<div class="col-sm-4">
+									<input name="correo" type="email" class="form-control form-text" placeholder="Correo" required />
+								</div>
 
-					echo "<br />Su solicitud ha sido enviada y sera atendida a la brevedad posible.";
+							</div>
+							<div class="row">
 
-					 ?>
+								<div class="col-sm-12">
+									<textarea name="comentario" class="form-control" rows="4" placeholder="Comentario" required></textarea>
+									<button type="" class="btn btn-success"><span class="glyphicon glyphicon-send"></span> Enviar</button>
+								</div>
+
+							</div>
+						</form>
+
+						<?php
+						$nombre = $_POST['nombre'];
+						$apellido = $_POST['apellido'];
+						$email = $_POST['email'];
+						$comentario = $_POST['comentario'];
+
+						# Include the Autoloader (see "Libraries" for install instructions)
+						require 'vendor/autoload.php';
+						use Mailgun\Mailgun;
+
+						# Instantiate the client.
+						$mgClient = new Mailgun('key-bb6be62102fbde6cb67a14a09ef126fe');
+						$domain = "mg.delishotsdesserts.com";
+
+						# Make the call to the client.
+						$result = $mgClient->sendMessage($domain, array(
+								'from'    => 'Grupo JGM Web <postmaster@mg.delishotsdesserts.com>',
+								'to'      => 'Grupo JGM <jdsosa@gmail.com>',
+								'subject' => 'Nuevo mensaje desde la página web',
+								'text'    => '
+										Ha recibido un nuevo mensaje desde la página web.
+
+										Nombre: ' .$nombre .'
+										Apellido: ' .$apellido .'
+										Email: ' .$email .'
+										Comentario: ' .$comentario .'
+								'
+						));
+
+						echo "<br />Su solicitud ha sido enviada y sera atendida a la brevedad posible.";
+
+						 ?>
+						 
+					</div>
 
 				</div>
-
 			</main>
 		</div>
 	</div>
